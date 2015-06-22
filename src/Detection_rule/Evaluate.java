a6012e7335d3a667806e40268f21d16160b0b434
mabroukachouchane <chouchane.mabrouka@gmail.com>
2015-06-22 12:33:44 +0100

    updating

--- src/Detection_rule/Evaluate.java
+++ src/Detection_rule/Evaluate.java
@@ -1,6 +1,89 @@
 package Detection_rule;
 
-public class Evaluate {
-	
 
+import jxl.*;
+import jxl.read.biff.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import jxl.write.WriteException;
+
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class Evaluate {
+    public static void main(String[] args) throws IOException {
+    	int[] H = new int[22];
+    	int [] W = new int[22];
+            		try {
+                        String csvFile = "Trace/Trace.xlsx";
+                        String delimiter = ";";
+                        String line = null;
+                        StringTokenizer strToken = null;
+                        BufferedReader bufferReader;
+                        int lineID = 0;
+                        int fieldID = 0;
+                        int i=0;
+                        int j=0;
+                        double somme =0;
+                        int Interface = 540000;
+                        double area=0.0;
+                        //String[] T = new String[22];
+            //Ouvrir le fichier CSV 
+                        bufferReader = new BufferedReader(new FileReader(csvFile));
+            //parcourir les lignes du fichier CSV
+                        while ((line = bufferReader.readLine()) != null) {
+                            lineID++;
+            //Parcourir les champs séparés par delimiter
+                            strToken = new StringTokenizer(line, delimiter);
+                            while (strToken.hasMoreTokens()) {
+                            	
+                                fieldID++;
+                                if (fieldID==4)
+                                { System.out.println(strToken.nextToken()); 
+                                  //T[i]= strToken.nextToken() ;
+
+
+                                  System.out.println(H [i]);
+                                  //i++;    
+                                }
+                                else
+                                if (fieldID==5)
+                                { //System.out.println(strToken.nextToken()); 
+                                  W[j]= Integer.parseInt(strToken.nextToken()) ;
+                                  System.out.println(W[j]);
+                                  //j++;  
+                                   area = H [i] * W [i];  
+                                  System.out.println(area);
+                                  
+                                } 
+                               
+                                else
+                                {System.out.println("Ligne " + lineID
+                                        + " / champs " + fieldID
+                                        + " : " + strToken.nextToken());}  
+                                
+                               
+                            }
+                            somme+=area;
+                            System.out.println(" somme="+somme);
+                            //double DM = (double)1 - 2* Math.abs(0.5-(somme/Interface));
+                            //System.out.println("La densiter de l'interface est:"+DM);
+                            fieldID = 0;          
+                        }
+                        double DM = (double)1 - 2* Math.abs(0.5-(somme/Interface));
+                        System.out.println("La densiter de l'interface est:"+DM);
+                       /*BufferedWriter out =  new BufferedWriter (new FileWriter ("fichier.txt")) ;
+                        out.write(  DM);
+                        out.close();*/
+        } catch (IOException ex) {
+        	System.out.println("Erreur");
+        }           
+    }
+    
 }
+
+
--- src/Main/RuleMain.java
+++ src/Main/RuleMain.java
@@ -1,11 +1,27 @@
 
 package Main;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-
 import java.util.Random;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Iterator;
 
+import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.xssf.*;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+ 
+
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 import Detection_rule.Input;
-
 import Detection_rule.MetaModel;
 import Detection_rule.Rule;
 public class  RuleMain 
@@ -67,8 +83,198 @@ public class  RuleMain
           System.out.println("\n leaf number "+(i+1)+" : "+rules.get(i).rule_text);
       }
   }
+
+ void evaluate_rules() throws FileNotFoundException, IOException{
+	 
+	 
+
+	 
+	 
+	 
+	 
+	 
+	 
+Workbook workbook = null;
+
+		/* Récupération du classeur Excel (en lecture) */
+		try {
+			workbook = Workbook.getWorkbook(new File("Trace/Trace.xls"));
+		} catch (BiffException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/* Un fichier excel est composé de plusieurs feuilles, on y accède de la manière suivante*/
+		Sheet sheet =  workbook.getSheet(0);
+		 
+		/*final int FIN = 10; //Le nombre que tu souhaites ici
+		for(int i = 0; i < FIN; i++){
+		Cell cell = (Cell) sheet.getCell(0, i);
+		String test = ((jxl.Cell) cell).getContents();
+		System.out.println(test);*/
+		
+		// On accède aux cellules avec la méthode getCell(indiceColonne, indiceLigne) 
+		jxl.Cell a1 = sheet.getCell(0,0); 
+		
+		/* On peut également le faire avec getCell(nomCellule)
+		Cell c5 = sheet.getCell("C5"); */
+		
+		/* On peut récupérer le contenu d'une cellule en utilisant la méthode getContents() */
+		String contenuA1= a1.getContents();
+		//String contenuC5 = c5.getContents();
+		
+		System.out.println(contenuA1);
+		//System.out.println(contenuC5);
+	
+ }
+	
+	
+ 
+   /*  individual_quality = 0 ;
+     individual_complexity = 0 ;
+     tables_number = 0;
+     columns_number = 0;
+     primary_keys_number = 0;
+     correct_rules = new ArrayList<Integer>();
+     this.evaluation_empty_table();
+     this.evaluation_primary_foreign_keys();
     
-  public static void main (String[] args)  
+     
+     // tables number :
+     for (int i=0; i<output.size();i++)
+             {
+                 if(output.get(i).prefixes.get(0).getText().equals("Table"))
+                 {
+                     tables_number++;
+                 }
+             }
+     // columns number :
+     for (int i=0; i<output.size();i++)
+             {
+                 if(output.get(i).prefixes.get(0).getText().equals("Column"))
+                 {
+                     columns_number++;
+                 }
+             }
+     // primary keys number :
+     for (int i=0; i<output.size();i++)
+             {
+                 if(output.get(i).suffixes.size()>1)
+                 {
+                     if(output.get(i).suffixes.get(output.get(i).suffixes.size()-1).getText().equals("pk"))
+                     {
+                         primary_keys_number++;
+                     }
+                 }  
+             }
+     int max1 ;
+     if(tables_number>(metrics.tables_lower_bound + (tables_number*this.rules.size()))) max1 = tables_number ; else max1 = metrics.tables_lower_bound + this.rules.size() ;
+     int max2 ;
+     if(columns_number>(metrics.columns_lower_bound + (columns_number*this.rules.size()))) max2 = columns_number ; else max2 = metrics.columns_lower_bound + this.rules.size() ;
+     int max3 ;
+     if(primary_keys_number>(metrics.primary_keys_lower_bound + (primary_keys_number*this.rules.size()))) max3 = primary_keys_number ; else max3 = metrics.primary_keys_lower_bound + this.rules.size() ;
+     
+     individual_quality = Math.abs(metrics.tables_lower_bound );
+     individual_quality += Math.abs(metrics.columns_lower_bound );
+     individual_quality +=Math.abs(metrics.primary_keys_lower_bound );
+     individual_quality /= (double) (tables_number+columns_number+primary_keys_number) ;
+     //System.out.print("\n number of detected tables : "+tables_number);
+     //System.out.print("\n number of detected columns : "+columns_number);
+     //System.out.print("\n number of expected tables : "+metrcis.tables_lower_bound);
+     //System.out.print("\n number of expected columns : "+metrcis.columns_lower_bound);
+     //System.out.print("\n Individual Quality : "+individual_quality);
+     
+     this.complexity_calc();
+     
+     // Checking rules unprecision :
+     
+      for (int i=0; i<rules.size();i++)
+      {
+         //System.out.println("\n checking correct rules number at iteration : "+(i+1)+" is "+correct_rules.size());
+         if((rules.get(i).src.prefixes.get(0).getText().equals("Class"))&&rules.get(i).trg.prefixes.get(0).getText().equals("Table"))
+         {
+             correct_rules.add(i);
+             //System.out.println("\n class - table working");
+         }
+         else if((rules.get(i).src.prefixes.get(0).getText().equals("Attribute"))&&(rules.get(i).src.suffixes.get(2).getText().equals("unique")))
+         {
+             if((rules.get(i).trg.prefixes.get(0).getText().equals("Column"))&&(rules.get(i).trg.suffixes.get(2).getText().equals("pk")))
+             {
+                 correct_rules.add(i);
+                 //System.out.println("\n att unique working");
+             }
+
+         }
+         else if((rules.get(i).src.prefixes.get(0).getText().equals("Attribute"))&&(rules.get(i).src.suffixes.get(2).getText().equals("notunique")))
+         {
+             if((rules.get(i).trg.prefixes.get(0).getText().equals("Column"))&&(rules.get(i).trg.suffixes.get(2).getText().equals("_")))
+             {
+                 correct_rules.add(i);
+                 //System.out.println("\n att notunique working");
+             }
+         }
+         else if((rules.get(i).src.prefixes.get(0).getText().equals("Association"))&&(rules.get(i).src.suffixes.get(1).getText().equals("1")))
+         {
+             if((rules.get(i).trg.prefixes.get(0).getText().equals("Column"))&&(rules.get(i).trg.suffixes.get(2).getText().equals("fk")))
+             {
+                 correct_rules.add(i);
+                 //System.out.println("\n ass fk working");
+             }
+
+         }
+         else if((rules.get(i).src.prefixes.get(0).getText().equals("Association"))&&(rules.get(i).src.suffixes.get(1).getText().equals("n")))
+         {
+             if((rules.get(i).trg.prefixes.get(0).getText().equals("Column"))&&(rules.get(i).trg.suffixes.size()==4))
+             {
+                 correct_rules.add(i);
+                 //System.out.println("\n ass pfk working");
+             }
+         }
+         else if((rules.get(i).src.prefixes.get(0).getText().equals("Generalization")))
+         {
+             if((rules.get(i).trg.prefixes.get(0).getText().equals("Column"))&&(rules.get(i).trg.suffixes.get(2).getText().equals("pfk")))
+             {
+                 correct_rules.add(i);
+                 //System.out.println("\n generalization working");
+             }
+         }
+      }
+      this.correct_rules_ratio = (double)correct_rules.size()/rules.size();
+      
+      // calculating constraints
+      
+      double temp1 = 0 ;
+      double temp2 = 0 ;
+      
+      if (this.total_tables != 0)
+      {
+          temp1 = (double)(this.not_empty_table/this.total_tables);
+      }
+      if (this.total_fkeys != 0)
+      {
+          temp2 = (double)(this.fk_is_pk/this.total_fkeys);
+      }
+      
+      //System.out.println("\n not_empty_table/this.total_tables : "+temp1);
+      //System.out.println("\n this.fk_is_pk/this.total_fkeys : "+temp2);
+      
+      this.metamodel_constraints_ratio = (double) (temp1 + temp2 + 1.0 + 1.0) / 4.0 ;
+      
+      // unprecision :
+      
+      individual_unprecision = 1 - (0.75*metamodel_constraints_ratio + correct_rules_ratio*0.25) ;
+ }
+ 
+
+ 
+*/
+ 
+ 
+    
+  public static void main (String[] args) throws FileNotFoundException, IOException  
   {
         int min_rules_size = 30;
         int max_rules_size = 100
@@ -76,5 +282,6 @@ public class  RuleMain
         RuleMain R = new RuleMain();
         Input input = new Input();
        
-        R.create_rules(input, min_rules_size, max_rules_size);}
+        R.create_rules(input, min_rules_size, max_rules_size);
+        R.evaluate_rules();}
 }
